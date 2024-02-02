d17f968af37b4fcb75fcd72989ed78f14d32d90a
TessRoovers <93520713+TessRoovers@users.noreply.github.com>
2024-02-02 17:15:17 +0100

    Update job_all.sh

--- job_all.sh
+++ job_all.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # Set job requirements
-#SBATCH --job-name LOSSTEST
+#SBATCH --job-name TRAIN
 #SBATCH --partition=gpu
 #SBATCH --gpus=1
 #SBATCH --ntasks=1
 #SBATCH --nodes=1
-#SBATCH --time=3:00:00
+#SBATCH --time=32:00:00
 
 cp $HOME/evaluate.py "$TMPDIR"
 cp -r $HOME/unet "$TMPDIR"
@@ -21,27 +21,24 @@ module load CUDA/12.1.1
 source activate env2
 echo "Activation complete.."
 
-# PROBS=("0.001" "0.005" "0.01" "0.02" "0.03" "0.04" "0.05")
-# STDS=("0.01" "0.02" "0.03" "0.04" "0.05" "0.1" "0.2")
-# PARAMS=("A" "B" "C" "D" "E" "F" "G")
+PROBS=("0.001" "0.005" "0.01" "0.02" "0.03" "0.04" "0.05")
+STDS=("0.01" "0.02" "0.03" "0.04" "0.05" "0.1" "0.2")
+PARAMS=("A" "B" "C" "D" "E" "F" "G")
 
-# for ((i=0; i<7; i++)); do
-#     PROB=${PROBS[i]}
-#     STD=${STDS[i]}
-#     PARAM=${PARAMS[i]}
+for ((i=0; i<7; i++)); do
+    PROB=${PROBS[i]}
+    STD=${STDS[i]}
+    PARAM=${PARAMS[i]}
 
 
-#     python3 -uc "import torch; print('PyTorch:\n GPU available...?', torch.cuda.is_available(), '\n PyTorch version:', torch.__version__)"
-#     echo "Running training code for $PARAM..."
-#     python3 "$HOME/train_gc.py" --prob $PROB --std $STD --param $PARAM &
-#     echo "Training code for $PARAM started!"
+    python3 -uc "import torch; print('PyTorch:\n GPU available...?', torch.cuda.is_available(), '\n PyTorch version:', torch.__version__)"
+    echo "Running training code for $PARAM..."
+    python3 "$HOME/train_gc.py" --prob $PROB --std $STD --param $PARAM &
+    echo "Training code for $PARAM started!"
 
-# done
-# wait
+done
+wait
 
-python3 -uc "import torch; print('PyTorch:\n  * GPU available...?', torch.cuda.is_available(), '\n  * PyTorch version:', torch.__version__)"
-echo "Running training code..."
-python3 "$HOME/train_gc.py" --prob "0.005" --std "0.03" --param "LOSS"
 
 echo "Finished training for parameters."
 
